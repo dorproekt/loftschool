c9a6c8736d6d18efe4f2ef1dd07ba872e7557f79
Alexandr Gorban <dorproekt@ukr.net>
2017-05-13 09:39:04 +0300

    lesson 3 hw#1

--- tmp/dev/js/main.js
+++ tmp/dev/js/main.js
@@ -1,68 +1,48 @@
 /*
-1. # ДЗ 1:
-Написать функцию 'isAllTrue', которая принимает 2 параметра - 'source' и 'filterFn'
-source - массив 'filterFn' - фильтрующая функция
-Если фильтрующая функция вернет 'true' для ВСЕХ элементов массива, то и сама 'isAllTrue' вернет 'true'
-Если фильтрующая функция вернет 'false' хотя бы для одного элемента массива, то и сама 'isAllTrue' вернет 'false'
+
+2. # ДЗ - 2
+Написать фукнцию 'isSomeTrue', которая принимает 2 параметра - 'source' и 'filterFn'
+'source' - массив
+'filterFn' - фильтрующая функция
+Если фильтрующая функция вернет 'true' хотя бы для одного элемента массива, то и сама 'isSomeTrue' вернет 'true'
+Если фильтрующая функция вернет 'false' для ВСЕХ элементов массива, то и сама 'isSomeTrue' вернет 'false'
+
+Всё должно быть реализовано с использованием чистого js (не используя сторонние библиотеки).
+Так же, нельзя использовать функции для работы с массивами.
 
 пример:
-var allNumbers = [1, 2, 4, 5, 6, 7, 8],
-someNumbers = [1, 2, 'привет', 4, 5, 'loftschool', 6, 7, 8],
-noNumbers = ['это', 'массив', 'без', 'чисел'];
-
-function isNumber(val) {
-return typeof val === 'number';
-}
-
-console.log(isAllTrue(allNumbers, isNumber)); //вернет true
-console.log(isAllTrue(someNumbers, isNumber)); //вернет false
-console.log(isAllTrue(noNumbers, isNumber)); //вернет false
-
-Выбрасывать и обрабатывать исключение, если в 'source' пустой массив.
+console.log(isSomeTrue(allNumbers, isNumber)); //вернет true
+console.log(isSomeTrue(someNumbers, isNumber)); //вернет true
+console.log(isSomeTrue(noNumbers, isNumber)); //вернет false
 
 */
 
-function isAllTrue(source, filterFn){
-    //debugger;
+function isSomeTrue(source, filterFn){
     try{
         if(source.length === 0){
             throw new Error("Пустой массив!");
         }
         for(let i = 0; i < source.length; i++){
-        
-            if(!filterFn(source[i])){
-                return false;
+            if(filterFn(source[i])){
+                return true;
             }
         }
-        
-        return true;
+        return false;
     }catch(e){
-        console.error(e.message)
+        alert(e.message);
     }
 }
 
-
-let source = ["1", "2", "40", 50];
-let arr = [];
-
-function isNumber(val){
-    
-    if(typeof val !== 'number'){
+function filterFn(val){
+    if(typeof val !== "number"){
         return false;
     }
-    
     return true;
 }
 
+let source = [1,2];
+let res = isSomeTrue(source, filterFn);
 
-function isStr(val){
-    
-    if(typeof val !== 'string'){
-        return false;
-    }
-    
-    return true;
+if(res){
+    console.log(res);
 }
-
-let res = isAllTrue(arr, isNumber);
-console.log(res);
