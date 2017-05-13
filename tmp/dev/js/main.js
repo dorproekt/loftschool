e1eac7468940c6690d48a09d314b9e555654decb
Alexandr Gorban <dorproekt@ukr.net>
2017-05-13 10:13:42 +0300

    lesson 3 hw#3 calculator

--- tmp/dev/js/main.js
+++ tmp/dev/js/main.js
@@ -1,48 +1,89 @@
 /*
 
-2. # ДЗ - 2
-Написать фукнцию 'isSomeTrue', которая принимает 2 параметра - 'source' и 'filterFn'
-'source' - массив
-'filterFn' - фильтрующая функция
-Если фильтрующая функция вернет 'true' хотя бы для одного элемента массива, то и сама 'isSomeTrue' вернет 'true'
-Если фильтрующая функция вернет 'false' для ВСЕХ элементов массива, то и сама 'isSomeTrue' вернет 'false'
-
-Всё должно быть реализовано с использованием чистого js (не используя сторонние библиотеки).
-Так же, нельзя использовать функции для работы с массивами.
+3. # ДЗ 3 (со звездочкой)
+Написать функцию 'calculator' (в виде модуля), которая имеет один параметр - 'firstNumber'
+'firstNumber' - это число, с которым будут производиться действия
+Функция 'calculator' должна возвращать объект, у которого должно быть несколько функций.
+Каждая из этих функций принимает неограниченное количество аргументов и производит какие-то арифметические операции с этими аргументами и тем числом, которое было передано в 'calculator' и возвращает результат:
+- 'sum' - складывает 'firstNumber' с переданным аргументами
+- 'dif' - вычитает из 'firstNumber' переданные аргументы
+- 'div' - делит 'firstNumber' на первый переданный аргумент. Результат этой операции делится на второй переданный аргумент (если он есть) и так далее
+- 'mul' - умножает 'firstNumber' на первый переданный аргумент. Результат этой операции умножается на второй переданный аргумент (если он есть) и так далее.
+Предусмотреть исключительные ситуации, для функции 'div', когда делитель равен нулю
 
 пример:
-console.log(isSomeTrue(allNumbers, isNumber)); //вернет true
-console.log(isSomeTrue(someNumbers, isNumber)); //вернет true
-console.log(isSomeTrue(noNumbers, isNumber)); //вернет false
+var myCalculator = calculator(100);
+
+console.log(myCalculator.sum(1, 2, 3)); //вернет 106
+console.log(myCalculator.dif(10, 20)); //вернет 70
+console.log(myCalculator.div(2, 2)); //вернет 25
+console.log(myCalculator.mul(2, 2)); //вернет 400
 
 */
 
-function isSomeTrue(source, filterFn){
-    try{
-        if(source.length === 0){
-            throw new Error("Пустой массив!");
-        }
-        for(let i = 0; i < source.length; i++){
-            if(filterFn(source[i])){
-                return true;
-            }
-        }
-        return false;
-    }catch(e){
-        alert(e.message);
+(function(){
+    
+    function calculator(firstNumber){
+        return {
+            
+            firstNumber: firstNumber,
+            
+            sum: function(){
+                let result = 0;
+                
+                for(let i = 0; i < arguments.length; i++){
+                    result += arguments[i];
+                }
+                
+                return result + this.firstNumber;
+            },
+            
+            dif: function(){
+                let result = this.firstNumber;
+                
+                for(let i = 0; i < arguments.length; i++){
+                    result -= arguments[i];
+                }
+                
+                return result;
+            },
+            
+            div: function(){
+                let result = this.firstNumber;
+                
+                for(let i = 0; i < arguments.length; i++){
+                    try{
+                        if(arguments[i] === 0){
+                            throw new Error("На ноль делить нельзя!!!");
+                        }
+                        result /= arguments[i];   
+                    }catch(e){
+                        console.log(e.message);
+                    }
+                }
+                
+                return result;
+            },
+            
+            mul: function(){
+                let result = this.firstNumber;
+                
+                for(let i = 0; i < arguments.length; i++){
+                    result *= arguments[i];
+                }
+                
+                return result;
+            },
+            
+        };
     }
-}
-
-function filterFn(val){
-    if(typeof val !== "number"){
-        return false;
-    }
-    return true;
-}
-
-let source = [1,2];
-let res = isSomeTrue(source, filterFn);
-
-if(res){
-    console.log(res);
-}
+    
+    var myCalculator = calculator(100);
+    
+    console.log(myCalculator.sum(2,6,2));
+    console.log(myCalculator.dif(12,36,25));
+    console.log(myCalculator.div(2, 0, 25));
+    console.log(myCalculator.mul(2,2,4));
+    //console.log(myCalculator.firstNumber);
+    
+})();
