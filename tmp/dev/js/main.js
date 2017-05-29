b458692d53c938f2dff3e1f5b279e0f27e221d6b
Alexandr Gorban <dorproekt@ukr.net>
2017-05-29 11:36:24 +0300

    lesson 8 hw#2

--- tmp/dev/js/main.js
+++ tmp/dev/js/main.js
@@ -1,27 +1,50 @@
 /*
 
-1. ДЗ 1:
-Создать модуль, который экспортирует функцию `timer`.
-Функция `timer` должна возвращать новый промис.
-Функция `timer` принимает 1 аргумент - количество миллисекунд, через которые промис должен перейти в состояние `fulfilled`.
-Пример использования:
-timer(3000).then(() => console.log('я вывелась через 3 секунды'))
+2. ДЗ 2:
+Загрузить города при помощи AJAX из https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json (сервер поддерживает AJAX CORS)
+Отсортировать города по алфавиту и вывести на странице.
+Использование промисов обязательно.
+Запрещено использование любых библиотек (включая jQuery) и фреймворков.
 
 */
 
-function timer(time){
-    return promise = new Promise(function(resolve, reject){
-        if(time){
-            setInterval(() => {
-                resolve(time);
-            }, time);
-        }else{
-            reject();
-        }
-    });
+
+function getText(){
+   return new Promise((resolve, reject)=>{
+       let xhr = new XMLHttpRequest();
+
+        xhr.open("GET", "https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json");
+
+        xhr.onload = function() {  
+            if(xhr.response){
+                resolve(xhr.response);
+            }else{
+                reject("Ошибка");
+            }
+        };
+
+        xhr.send(); 
+   });
 }
 
 
-timer(5000).then(function(val){
-    console.log("Я вызвалась через "+val/1000+" с.");
+myButton.addEventListener("click", ()=>{
+    getText().then((val)=>{
+       
+        let citys = JSON.parse(val);
+        let sortCitys = [];
+        
+        for(let item of citys){
+            sortCitys.push(item["name"]);
+        }
+        sortCitys = sortCitys.sort();
+        
+        let container = document.querySelector(".citys");
+        for(let item of sortCitys){
+            container.innerHTML += "City - "+item+"<br><hr>";
+        }
+        
+    }, (val)=>{
+        console.log("Error");
+    });
 });
