f90ac14bf39df19c064b7fd168f1a7fdd88542a5
Alexandr Gorban <dorproekt@ukr.net>
2017-05-29 14:57:46 +0300

    lesson 8 hw#3*

--- tmp/dev/js/main.js
+++ tmp/dev/js/main.js
@@ -1,50 +1,72 @@
 /*
 
-2. ДЗ 2:
-Загрузить города при помощи AJAX из https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json (сервер поддерживает AJAX CORS)
-Отсортировать города по алфавиту и вывести на странице.
+3. ДЗ 3 (со звездочкой):
+Создать страничку с текстовым полем.
+После загрузки странички, загрузить список городов при помощи AJAX.
+При вводе текста в тестовое поле, выводить под текстовым полем список тех городов, в названиях которых есть введенный текст.
 Использование промисов обязательно.
 Запрещено использование любых библиотек (включая jQuery) и фреймворков.
 
 */
 
 
-function getText(){
-   return new Promise((resolve, reject)=>{
-       let xhr = new XMLHttpRequest();
+let myInput = document.getElementById("myInput");
+let href = "https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json";
+let container = document.querySelector("ul.result-search");
 
-        xhr.open("GET", "https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json");
-
-        xhr.onload = function() {  
-            if(xhr.response){
-                resolve(xhr.response);
-            }else{
-                reject("Ошибка");
-            }
-        };
-
-        xhr.send(); 
-   });
+function removeValue(e){
+    e.target.setAttribute("value", "");
 }
 
-
-myButton.addEventListener("click", ()=>{
-    getText().then((val)=>{
+function getCity(href){
+    return new Promise((resolve, reject) => {
+        
+        let xhr = new XMLHttpRequest();
+        
+        if(xhr){
+            xhr.open("GET", href);
+            xhr.onload = () => {
+                resolve(xhr.responseText);
+            };
+            xhr.send();
+        }else{
+            reject("err");
+        }
        
-        let citys = JSON.parse(val);
-        let sortCitys = [];
+    });
+}
+
+myInput.addEventListener("focus", removeValue);
+
+window.addEventListener("load", () => {
+    getCity(href).then((val) => {
         
-        for(let item of citys){
-            sortCitys.push(item["name"]);
+        let result = JSON.parse(val);
+        
+        let arrCity = [];
+        for(let city of result){
+            arrCity.push(city["name"].toLowerCase());
         }
-        sortCitys = sortCitys.sort();
+
+        myInput.addEventListener("keypress", (e) => {
+            container.innerHTML = "";
+            let searchValue = e.target.value + e.key;
+            let resArr = [];
+            for(let item of arrCity){
+                if(item.substr(0, searchValue.length) === searchValue){
+                    resArr.push(item);
+                }
+            }
+            if(resArr.length){
+                for(let item of resArr){
+                    container.innerHTML += "<li><a href='#"+item+"'>"+item+"</a></li>"; 
+                }
+            }
+            
+        });
         
-        let container = document.querySelector(".citys");
-        for(let item of sortCitys){
-            container.innerHTML += "City - "+item+"<br><hr>";
-        }
         
-    }, (val)=>{
-        console.log("Error");
+    }, (value) => {
+         console.log(value);
     });
 });
