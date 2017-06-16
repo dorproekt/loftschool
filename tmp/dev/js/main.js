fc54dd8207a7062a6198feb506f36cc4f2256868
Alexandr Gorban <dorproekt@ukr.net>
2017-06-16 11:13:44 +0300

    lesson 12 HTML 5 API history.pushState

--- tmp/dev/js/main.js
+++ tmp/dev/js/main.js
@@ -1,69 +1,35 @@
-Handlebars.registerHelper('formatTime', time => {
-    let minutes = parseInt(time/60);
-    let seconds = time - minutes*60;
-    
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    
-    return minutes + ':' + seconds;
+let save = document.querySelector("#save");
+let fSize = document.querySelector("#fS");
+let bgColor = document.querySelector("#bgC");
+let fontColor = document.querySelector("#fC");
+let wrap = document.querySelector("#wrap");
+
+document.addEventListener("keyup", e => {
+    applyStyles();
 });
 
-
-Handlebars.registerHelper('dateFormat', date => {
-    let resDate = new Date(date);
-    return resDate;
+save.addEventListener("click", () => {
+    let url = "/"+fSize.value+"/"+bgColor.value+"/"+fontColor.value;
+    //console.log(url);
+    history.pushState({
+        fSize: fSize.value,
+        bgColor: bgColor.value,
+        fontColor: fontColor.value
+    }, "new", url);  
 });
 
-new Promise((resolve) => {
-    if(document.readyState == "complete"){
-        resolve();
-    }else{
-        window.addEventListener("load", resolve);
-    }
-}).then(() => {
-    return new Promise((resolve, reject) => {
-        VK.init({  
-            apiId: 6054796
-        });
+window.addEventListener("popstate", e => {
+    console.log(e);
+    
+    fSize.value = e.state.fSize;
+    bgColor.value = e.state.bgColor;
+    fontColor.value = e.state.fontColor;
+    
+    applyStyles();
+});
 
-        VK.Auth.login(function(response) {
-            if (response.session) {
-                resolve(response);
-            } else { 
-                reject(new Error("Не удалось авторизироваться"));
-            } 
-        }, 8 | 4 | 16);
-    });
-}).then(() => {
-    return new Promise((resolve, reject) => {
-        VK.api("users.get", {"name_case" : "gen"}, response => {
-            if(response.error){
-                reject(new Error(response.error.error_msg));
-            }else{
-                let userData = response.response[0];
-                headerInfo.innerHTML = "Видео на странице "+userData.first_name+" "+userData.last_name;
-                resolve();
-            }
-        });
-
-    });
-}).then(() => {
-    return new Promise((resolve, reject) => {
-        VK.api("video.get", {}, response => {
-            if(response.error){
-                reject(new Error(response.error.error_msg));
-            }else{ 
-                response.response.shift();
-                //console.log(response.response);
-                let sourse = document.querySelector("#videoVkTmp").innerHTML;
-                let templateFn = Handlebars.compile(sourse);
-                let template = templateFn({list: response.response})
-                
-                results.innerHTML = template;
-                resolve();
-            }
-        });
-    });
-}).catch((e) => {
-    console.error(e.message);
-});  
+function applyStyles(){
+    wrap.style.fontSize = fSize.value;
+    wrap.style.backgroundColor = bgColor.value;
+    wrap.style.color = fontColor.value;
+}
