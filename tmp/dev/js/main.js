c440f79d31c1ddda8c2e5986c463d2cd5b1525e0
Alexandr Gorban <dorproekt@ukr.net>
2017-06-26 15:24:21 +0300

    template

--- tmp/dev/js/main.js
+++ tmp/dev/js/main.js
@@ -1,27 +1,12 @@
-let save = document.querySelector("#save");
-let load = document.querySelector("#load");
+let getLocation = document.querySelector("#getLocation");
 
-save.addEventListener("click", (e) => {
-    e.preventDefault();
-    
-    let name = document.querySelector("#name").value;
-    let lastName = document.querySelector("#lastName").value;
-    let text = document.querySelector("#text").value;
-    
-    localStorage.someData = JSON.stringify({
-        name: name,
-        lastName: lastName,
-        text: text
+getLocation.addEventListener("click", () => {
+    navigator.geolocation.getCurrentPosition(addPos, () =>{
+        alert("Err!!");
     });
-
 });
 
-load.addEventListener("click", e => {
-    e.preventDefault();
-    let data = JSON.parse(localStorage.someData);
-    //console.log(data);
-    
-    document.querySelector("#name").value = data.name;
-    document.querySelector("#lastName").value = data.lastName;
-    document.querySelector("#text").value = data.text;
-})
+function addPos(e){
+    console.log(e.coords.latitude, e.coords.longitude);
+    console.log(e);
+}
